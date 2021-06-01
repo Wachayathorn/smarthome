29d0dcca51ef76c92ab58f4e0e9a477867953634
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-06-01 10:57:59 +0700

    feat: update database connection

--- src/shared/database/database.config.ts
+++ src/shared/database/database.config.ts
@@ -3,11 +3,12 @@ import * as path from 'path';
 export default () => ({
     typeormConnection: {
         type: process.env.TYPEORM_TYPE,
-        host: process.env.TYPEORM_HOST,
-        port: process.env.TYPEORM_PORT,
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE,
+        // host: process.env.TYPEORM_HOST,
+        // port: process.env.TYPEORM_PORT,
+        url: `${process.env.TYPEORM_TYPE}://${process.env.TYPEORM_USERNAME}:${process.env.TYPEORM_PASSWORD}@${process.env.TYPEORM_HOST}:${process.env.TYPEORM_PORT}/${process.env.TYPEORM_DATABASE}`,
+        // username: process.env.TYPEORM_USERNAME,
+        // password: process.env.TYPEORM_PASSWORD,
+        // database: process.env.TYPEORM_DATABASE,
         entities: [path.resolve(__dirname, '../entities/*{.ts,.js}')],
         synchronize: false,
     },
